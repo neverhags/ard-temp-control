2d558f3ef7af9228b9e4803cf73eb59fe5633374
Heilner Garcia <neverhags@gmail.com>
2020-03-30 17:10:01 -0500

    feat: plotter view

--- temp-control.ino
+++ temp-control.ino
@@ -1,34 +1,47 @@
-// termometro múltiple fácil de calibrar
-float termometro1= 0;// entrada A0
+#include <PID_v1.h>
 
-int tiempo=0;
-int cnt=0;
+/* Conversion */
+const int maxTempCalib = 510;
+const int minTempCalib = 670;
+
+/* Time vars */
+unsigned long actTime = 0;
+unsigned long readTempTimeout = 0;
+
+/* Temp vars */
+unsigned long iterations = 0;
+float poolValues = 0.0;
+
+int scanTime = 1; // Seconds
+const int millisMultipler = 1000;
+double Setpoint, Input, Output;
 float t1=0;
 
-/* time variables */
-unsigned long actTime = 0;
-unsigned long 
 
 void setup() {
   Serial.begin(9600); 
-  analogReference(INTERNAL);// pone como referencia iterna 1.1V
+  analogReference(INTERNAL);
   pinMode(A0, OUTPUT);
   pinMode(A2, OUTPUT);
   pinMode(A1, INPUT);
   digitalWrite(A0, HIGH);
   digitalWrite(A2, LOW);
 }
-void loop() {
-  
-   t1 = map(analogRead(1),472,638,1000,0); // a 0°C la lectrura de analgRead es 140 y a 100°C es 99
-   
-   termometro1= t1/10;
-   
-   tiempo =millis()/1000;
-   // para ver en el monitor
-Serial.print (tiempo);
-Serial.print (" Term 1= ");   
-Serial.println (termometro1,1);
 
- delay(1000); 
+void loop() {
+  actTime = millis();
+  if (actTime >= readTempTimeout) {
+    // para ver en el monitor
+    Serial.println ((poolValues/iterations),1);
+    reset();
+    readTempTimeout = actTime + (millisMultipler * scanTime);
+  } else {
+    poolValues += map(analogRead(1), maxTempCalib, minTempCalib, 1000,0) / 10;
+    iterations++;
+  }
+}
+
+void reset() {
+  poolValues = 0;
+  iterations = 0;
 }
